39bddf7e35882a1c7ccf74751db66cea0003da1c
Sambhawi Raj Nandini <119286849+Sambhawii@users.noreply.github.com>
2023-02-21 20:59:35 +0530

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,12 @@
+first=int(input("enter the first number"))
+second=int(input("enter the second number"))
+temp=first;
+first=second;
+second=temp;
+
+print("after swapping number is")
+print(first,second)
+
+
+
+    
